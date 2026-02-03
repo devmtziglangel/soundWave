9b46c36e87b069704b7bcc2cff0c400815ab42d4
devmtziglangel <dev.angelmartinez@gmail.com>
2026-02-03 20:02:10 +0100

    terminada clase contenido

--- src/modelo/contenido/Contenido.java
+++ src/modelo/contenido/Contenido.java
@@ -1,21 +1,25 @@
 package modelo.contenido;
 
+import excepciones.contenido.ContenidoNoDisponibleException;
 import excepciones.contenido.DuracionInvalidaException;
 import interfaces.Descargable;
 import interfaces.Reproducible;
+import modelo.plataforma.Playlist;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 public abstract  class Contenido   implements Descargable, Reproducible {
     //ATRIBUTOS
     protected String id;
     protected String titulo;
+    protected int reproducciones;
     protected int likes;
     protected  int duracionSegundos;
     protected ArrayList<String> tags;
-    protected boolean disponiles;
+    protected boolean disponible;
     protected Date fechaPublicacion;
 
     //CONSTRUCTOR
@@ -26,8 +30,147 @@ public abstract  class Contenido   implements Descargable, Reproducible {
 
         this.id = UUID.randomUUID().toString();
         this.titulo = titulo;
-        this.likes
+        this.reproducciones = 0;
+        this.likes = 0;
         this.duracionSegundos = duracionSegundos;
+        this.tags = new ArrayList<>();
+        this.disponible = true; //Iniciarlo en true o en false;
+        this.fechaPublicacion = new Date();
 
     }
+
+    //GETs Y SETTERs
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getReproducciones(){
+        return reproducciones;
+    }
+
+    public void setReproducciones(int reproducciones){
+        this.reproducciones = reproducciones;
+    }
+
+    public int getLikes(){
+        return likes;
+    }
+
+    public int getDuracionSegundos(){
+        return duracionSegundos;
+    }
+
+    public ArrayList<String>getTags(){  // defensivo
+        return new ArrayList<>(tags);
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public Date getFechaPublicacion(){
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(Date fechaPublicacion){
+         this.fechaPublicacion = fechaPublicacion;
+    }
+
+    //METODO Abstracto
+    public abstract void reproducir() throws ContenidoNoDisponibleException;
+
+    //METODO CONCRETO
+
+    public void aumentarReproducciones(){
+        this.reproducciones++;
+    }
+
+    public void agregarLike (){
+        this.likes++;
+    }
+
+    public boolean esPopular(){
+        return reproducciones>100000;
+    }
+
+    public  void validarDuracion() throws DuracionInvalidaException{
+        if (this.duracionSegundos<=0){
+            throw new DuracionInvalidaException("Duracion invalida " + this.duracionSegundos);
+        }
+    }
+
+    public void agregarTags(String tag) {
+        if (tag != null && !tags.contains(tag)){
+           this.tags.add(tag);
+        }
+    }
+
+    public boolean tieneTag (String tag){
+        return tags.contains(tag);
+    }
+
+    public void  marcarNoDisponible(){
+        this.disponible = false;
+    }
+
+    public void  marcarDisponible(){
+        this.disponible = true;
+    }
+
+    public String getDuracionFormateada(){
+        int min = duracionSegundos /60;
+        int seg = duracionSegundos %60;
+        return String.format("%d:%02d", min, seg);
+    }
+
+
+
+    //OVERRIDES
+
+    @Override
+    public String toString() {
+        return this.titulo + " (" + getDuracionFormateada() + ")";
+    }
+
+    @Override
+    public int hashCode(){
+        return this.id.hashCode();
+    }
+
+    @Override
+    public boolean equals (Object obj){
+        if(this == obj ){
+            return true;
+        }
+
+        if (obj == null || getClass() !=obj.getClass()){
+            return  false;
+        }
+        Contenido otro = (Contenido) obj;
+        return  this.id.equals(otro.id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
